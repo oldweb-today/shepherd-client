9063012c171cf59e6c511ec928f6bb6c8fa2fa34
Ilya Kreymer <ikreymer@gmail.com>
2018-12-17 16:12:50 -0800

    audio init: init audio immediately unless 'audio' response is 'wait_for_click' bundle update: add bundle-dev to build in dev mode, bundle to build prod, tweak README bundle update: always include inline source map (prod and dev) for debugging dist: build prod with source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './index.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'shepherd-client.bundle.js',
